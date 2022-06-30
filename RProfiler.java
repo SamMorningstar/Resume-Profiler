77a654580b56d2cddb50a8b7ec07616f38c8833f
Sam Morningstar <SamMorningstar5064@gmail.com>
2022-06-30 08:29:47 +0400

    UI Setup Done

--- RProfiler.java
+++ RProfiler.java
@@ -5,18 +5,46 @@ import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import javax.swing.*;
+import java.awt.*;
 
 class RProfiler {
     public static void main(String[] args){
         JFrame frame = new JFrame("Resume Profiler");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700, 500);
+        frame.setSize(700, 200);
+        frame.setLayout(new GridLayout(3, 1));
+
+        JPanel upPanel = new JPanel();
+        JLabel upLabel = new JLabel("Upload Resume you want to scan");
+        JButton upButton = new JButton("Upload Resume");
+        upPanel.add(upLabel);
+        upPanel.add(upButton);
+
+        JPanel keyPanel = new JPanel();
+        JLabel keyLabel = new JLabel("Enter the Keywords you want to scan(Seperated by commas(,))");
+        JTextField keyTxt = new JTextField(40);
+        keyPanel.add(keyLabel);
+        keyPanel.add(keyTxt);
+
+        JPanel resPanel = new JPanel();
+        JLabel resLabel = new JLabel("Are the Keywords found?");
+        JLabel resYLabel = new JLabel("Yes");
+        JLabel resNLabel = new JLabel("No");
+        resPanel.add(resLabel);
+        resPanel.add(resYLabel);
+        resPanel.add(resNLabel);
+        resYLabel.setVisible(false);
+        resNLabel.setVisible(false);
+
+        frame.getContentPane().add(upPanel);
+        frame.getContentPane().add(keyPanel);
+        frame.getContentPane().add(resPanel);
         frame.setVisible(true);
     }
 
     public void Profiler() throws IOException{
         ArrayList<String> keyword = new ArrayList<String>();
-        File resumeFile = new File("Files/Complete+Resume.pdf");
+        File resumeFile = new File("Files/");
         int alIndex = 0;
         Boolean checker;
         PDDocument document = Loader.loadPDF(resumeFile);
